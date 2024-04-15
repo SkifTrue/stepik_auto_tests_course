bb13ded120774f666e5be588827ba7176aef7a1f
SkifTrue <vasili.trutnev@gmail.com>
2024-04-15 22:25:55 +0500

    Ожидание нужного текста на странице

--- code/2.4.7.py
+++ code/2.4.7.py
@@ -1,3 +1,4 @@
+# Ожидание нужного текста на странице
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
